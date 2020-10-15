6e22e94754c13c6e1654838134b13ac67e0ac03f
Guilherme Santos <guilherme.santos@doclerholding.com>
2020-10-15 14:23:06 +0200

    chore: adding some custom rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,57 @@
 module.exports = {
   extends: ['airbnb-typescript-prettier', 'prettier/standard'],
+  rules: {
+    'react/jsx-handler-names': [
+      'error',
+      {
+        eventHandlerPrefix: 'handle',
+        eventHandlerPropPrefix: 'on',
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 0, maxEOF: 1 }],
+    curly: ['error', 'all']
+  },
+  overrides: [
+    {
+      files: ['**/*.test.ts', '**/*.test.tsx'],
+      env: {
+        jest: true,
+      },
+      extends: ['plugin:jest-dom/recommended', 'plugin:testing-library/react'],
+      plugins: ['jest', 'jest-dom', 'testing-library'],
+      rules: {
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/prefer-to-have-length': 'warn',
+        'jest/valid-expect': 'error',
+      },
+    },
+  ],
 };
